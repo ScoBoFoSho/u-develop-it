aa0985729c497ec294cf086b3570624713e61a13
Cisco Ferre <ferrecisco@icloud.com>
2022-09-22 20:46:30 -0400

    Fixed .env

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+require('dotenv').config()
 
 // Connect to the database
 const db = mysql.createConnection(
@@ -7,10 +8,11 @@ const db = mysql.createConnection(
         // your MySQL username,
         user: 'root',
         // your mySQL password,
-        password: '!ManiPedi4Life!',
+        password: process.env.DB_PASSWORD,
         database: 'election'
     },
     console.log('Connected to the election database!')
 );
 
 module.exports = db;
+
